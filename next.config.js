4545f417bb4ea292351157c2887ec876526d4ac5
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-24 16:23:12 +0300

    added image domains to config file

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ["i.redd.it", "links.papareact.com", "linkedin.com"], // <== Domain name
+  },
+};
 
 module.exports = nextConfig
